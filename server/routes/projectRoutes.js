55193b38252c411b74b7ed1a81ef8085690c631e
yanmah-judix <yanmah@judix.in>
2026-02-22 21:33:38 +0530

    feat: add task route

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const Project=require('../models/Project');
+const Task=require('../models/Task');
 const auth=require('../middlewares/authMiddleware');
 
 router.post('/', auth, async (req,res)=>{
@@ -27,6 +28,24 @@ router.get('/',auth, async (req,res)=>{
         res.status(500).json({error:err.message});
     }
 })
+
+router.get('/:projectId', auth, async (req, res) => {
+  try {
+    const { projectId } = req.params;
+    const project = await Project.findById(projectId)   
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+    const tasks = await Task.find({ project: projectId });
+    res.json({
+      ...project.toObject(),
+      tasks
+    });
+  } 
+  catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 module.exports=router;
 
